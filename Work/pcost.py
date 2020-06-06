128b0b7057f82ca685ae2d90f89db1ffd5bf1f06
John Lim <yjlim90@gmail.com>
2020-06-05 23:39:56 -0700

    implement reading csv calculate stock cost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+import os
+
+total = 0
+
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+for line in f:
+    row = line.split(',')
+    total = total + int(row[1])*float(row[2][:-1])
+
+print(f'Total cost {total}')
+f.close()
