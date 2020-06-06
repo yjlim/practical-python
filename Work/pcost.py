919f57148eaa0badef8071b65731726505ee43c4
John Lim <yjlim90@gmail.com>
2020-06-05 23:56:15 -0700

    utilize csv library

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,17 +1,18 @@
 # pcost.py
 #
 # Exercise 1.27
-import os
+import csv
 
 def portfolio_cost(filename: str):
     with open(f'{filename}', 'rt') as f:
         total = 0
-        headers = next(f).split(',')
-        for line in f:
-            fields = line.split(',')
+        rows = csv.reader(f)
+        headers = next(rows)
+        for fields in rows:
+            #fields = line.split(',')
             try:
                 shares = int(fields[1])
-                cost = float(fields[2][:-1])
+                cost = float(fields[2])
             except ValueError:
                 print("Couldn't parse", line)
             total = total + shares*cost
